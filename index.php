3ca4f87ee7fd582e588115e2c7a024bd2b680e74
MSI <seroglazkinpavel@yandex.ru>
2020-12-10 21:37:01 +0400

    Добавлена картинка

--- index.php
+++ index.php
@@ -2,13 +2,18 @@
 <html lang="ru">
     <head>
         <meta charset="UTF-8">
-        <title>Урок 3</title>
+        <title>Урок 4</title>
     </head>
     <body>
-        <?php echo '<h1>Привет мир!</h1>
-            <a href="php/1.php">Переход на 1.php файл</a><br> 
-            <a href="php/2.php">Переход на 2.php файл</a><br>
-            <a href="/sct_php_1">Переход на index.php файл</a>'
-        ?>
+        <?='<h1>Привет мир!</h1>' ?>
+        <?="<p>Тема урока:'Базовый синтаксис php'.</p>" ?>
+        <?="<p>Необходимо выполнить задание.</p>" ?>
+        <a href="<?='php/1.php';?>">Переход на 1.php файл</a><br>
+        <a href="<?='php/2.php';?>">Переход на 2.php файл</a><br>
+        <a href="<?='/sct_php_1';?>">Переход на index.php файл</a><br>
+        <div style="margin-top:10px;"><img src="<? echo 'images/cats.gif';?>" alt="Картинка"></div>
     </body>
 </html>
+
+
+
--- php/2.php
+++ php/2.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Урок 3</title>
-</head>
-<body>
-<?php echo '<h2>Файл 2.php!</h2>
-            <a href="/sct_php_1/php/1.php">Переход на 1.php файл</a><br> 
-            <a href="/sct_php_1/php/2.php">Переход на 2.php файл</a><br>
-            <a href="/sct_php_1">Переход на index.php файл</a>';
-?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Урок 4</title>
+    </head>
+    <body>
+        <?='<h2>Файл 2.php!</h2>'; ?>
+            <a href="<?='/sct_php_1/php/1.php';?>">Переход на 1.php файл</a><br>
+            <a href="<?='/sct_php_1/php/2.php';?>">Переход на 2.php файл</a><br>
+            <a href="<?='/sct_php_1';?>">Переход на index.php файл</a>
+
+    </body>
 </html>
