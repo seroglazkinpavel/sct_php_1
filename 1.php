838012c3d501db864283fcce66a45282875809d8
MSI <seroglazkinpavel@yandex.ru>
2021-01-26 21:43:53 +0400

    Созданы новые два файла

--- 1.php
+++ 1.php
@@ -1,49 +1,25 @@
 <?php
-$a = rand(1, 20);
-switch($a){
-    case 1:
-        echo 1 . ' ';
-    case 2:
-        echo 2 . ' ';
-    case 3:
-        echo 3 . ' ';
-    case 4:
-        echo 4 . ' ';
-    case 5:
-        echo 5 . ' ';
-    case 6:
-        echo 6 . ' ';
-    case 7:
-        echo 7 . ' ';
-    case 8:
-        echo 8 . ' ';
-    case 9:
-        echo 9 . ' ';
-    case 10:
-        echo 10 . ' ';
-    case 11:
-        echo 11 . ' ';
-    case 12:
-        echo 12 . ' ';
-    case 13:
-        echo 13 . ' ';
-    case 14:
-        echo 14 . ' ';
-    case 15:
-        echo 15 . ' ';
-    case 16:
-        echo 16 . ' ';
-    case 17:
-        echo 17 . ' ';
-    case 18:
-        echo 18 . ' ';
-    case 19:
-        echo 19 . ' ';
-    default:
-        echo 20;
-        break;
+function ageAddressName($age, $address, $name)
+{
+    $result = (string)$age;
+    $rest = substr($result, -1);
+    switch ($rest) {
+        case 1:
+            echo "Возраст {$age} год, адресс {$address}, имя {$name}";
+            break;
+        case 2:
+            echo "Возраст {$age} года, адресс {$address}, имя {$name}";
+            break;
+        case 3:
+            echo "Возраст {$age} года, адресс {$address}, имя {$name}";
+            break;
+        case 4:
+            echo "Возраст {$age} года, адресс {$address}, имя {$name}";
+            break;
+        default:
+            echo "Возраст {$age} лет, адресс {$address}, имя {$name}";
+    }
 }
-echo '<hr>';
-for ($a ; $a <= 20; $a++) { // вопрос 5 (b-Чем в идеале следует заменить switch из первого задания для оптимально написанного кода?). Ответ циклом
-    echo $a.'<br>';
-}
+ageAddressName(57, 'Астрахань ул. Боевая д 36', 'Викор');
+
+
--- 2.php
+++ 2.php
@@ -1,29 +1,25 @@
 <?php
-$x1 = rand(-100, 100);
-$x2 = rand(-100, 100);
-$x3 = rand(-100, 100);
-if($x1 > $x2 && $x1 > $x3 && $x2 > $x3) {
-    echo $x3 . ' ' . $x2 . ' ' . $x1;
-}elseif($x1 > $x2 && $x1 > $x3 && $x2 < $x3) {
-    echo $x2 . ' ' . $x3 . ' ' . $x1;
-}elseif($x1 < $x2 && $x1 > $x3 && $x2 > $x3) {
-    echo $x3 . ' ' . $x1 . ' ' . $x2;
-}elseif($x1 < $x2 && $x1 < $x3 && $x2 > $x3) {
-    echo $x1 . ' ' . $x3 . ' ' . $x2;
-}elseif($x1 < $x2 && $x1 < $x3 && $x2 < $x3) {
-    echo $x1 . ' ' . $x2 . ' ' . $x3;
-}elseif($x1 > $x2 && $x1 < $x3 && $x2 < $x3) {
-    echo $x2 . ' ' . $x1 . ' ' . $x3;
-}elseif($x1 = $x2 && $x1 < $x3) {
-    echo $x2 . ' ' . $x1 . ' ' . $x3;
-}elseif($x1 = $x2 && $x1 > $x3) {
-    echo $x3 . ' ' . $x1 . ' ' . $x2;
-}elseif(($x1 > $x2) && ($x1 = $x3)) {
-    echo $x2 . ' ' . $x1 . ' ' . $x3;
-}elseif(($x1 < $x2) && ($x1 = $x3)) {
-    echo $x3 . ' ' . $x1 . ' ' . $x2;
-}elseif(($x3 = $x2) && ($x1 < $x3)) {
-    echo $x1 . ' ' . $x2 . ' ' . $x3;
-}elseif(($x3 = $x2) && ($x1 > $x3)) {
-    echo $x3 .' '. $x2 .' '. $x1;
+function getResult($a, $b, $result)
+{
+    switch ($result) {
+        case 'sum':
+            echo $a + $b;
+            break;
+        case sub:
+            echo $a - $b;
+            break;
+        case mult:
+            echo $a * $b;
+            break;
+        case div:
+            If($b == 0){
+                echo 'На 0 делить нельза';
+            }elseif($b != 0) {
+                echo $a / $b;
+            }
+            break;
+        default:
+            echo "Такого математического действия нет";
+    }
 }
+getResult(4, 1, 'div');
