edfa22a9a6f4a74905762e6c93b550fb16947788
MSI <seroglazkinpavel@yandex.ru>
2021-03-10 22:07:25 +0400

    Merge remote-tracking branch 'origin/lesson_22' into lesson_22
    # Conflicts:
    #	страницы/good-all.php

--- pages/auth-form.php
+++ pages/auth-form.php
@@ -1,6 +1,35 @@
+<?php
 
-<h1>Авторизация</h1>
-<form action="handler.php" method="post">
-    <input type="text" name="login">    
-    <button>Авторизоваться</button>
-</form>
+session_start();
+define('ADMIN', 'admin');
+
+if( isset($_GET['do']) && $_GET['do'] == 'exit' ) unset($_SESSION['admin']);
+
+
+if(empty($_SESSION['admin'])){
+echo '<h1>Авторизация</h1>
+			<form action="" method="post">
+				<input type="text" name="login">    
+				<button>Авторизоваться</button>
+			</form>';
+}else {$admin = $_SESSION['admin'] = ADMIN;
+ echo '<span style="color:green;"><b>Добро пожаловать, <b>'."$admin".'!</b> <a href="?page=auth-form&do=exit">Logout</a></b></span>';
+}	
+if(!empty($_POST['login'])){
+	if($_POST['login'] === ADMIN){
+		$admin = $_SESSION['admin'] = ADMIN;
+		
+		$_SESSION['res'] = '<span style="color:green;"><b>Добро пожаловать, <b>'."$admin".'!</b> <a href="?page=auth-form&do=exit">Logout</a></b></span>';
+		
+	}else{
+		
+		$_SESSION['res'] = '<p style="color:#ff0000;"><b>Неверный логин!</b></p><p>Повторите еще раз.</p>';
+		
+	}	
+}
+
+if(isset($_SESSION['res'])){
+	echo $_SESSION['res'];
+	unset($_SESSION['res']);
+}
+
