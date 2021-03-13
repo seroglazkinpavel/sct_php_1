071d6ac99ea58f0e80a500d29c64bee658b9874b
seroglazkinpavel <74137950+seroglazkinpavel@users.noreply.github.com>
2021-03-13 18:55:43 +0400

    Update auth-handler.php

--- auth-handler.php
+++ auth-handler.php
@@ -8,7 +8,7 @@ if(!empty($_POST['login']) && !empty($_POST['password'])){
 	if(trim($_POST['login']) === ADMIN && trim($_POST['password']) === PASSWORD){
 		$admin = $_SESSION['use'] = ADMIN;
 		$_SESSION['res'] = '<span style="color:green;"><b>Добро пожаловать, <b>'."$admin".'!</b> <a href="3.php?do=exit">Logout</a></b></span>';	
-	}else $_SESSIO['res'] = '<p style="color:#ff0000;"><b>Неверный логин или пароль!</b></p><a href="3.php">Logout</a>';
+	}else $_SESSION['res'] = '<p style="color:#ff0000;"><b>Неверный логин или пароль!</b></p><a href="3.php">Logout</a>';
 				
 }
 if(empty($_POST['login']) || empty($_POST['password'])){
